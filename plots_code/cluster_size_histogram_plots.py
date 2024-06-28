Fixing calculation code to account for type 5 proteins, running code, fixing plots and plotting plots.

plots_code/cluster_size_histogram_plots.py
@@ -71,11 +71,11 @@ def plot_histogram_models_123(cs_list_step_1, counts_list, model, color):
 
     plt.xlabel('Cluster sizes')
     plt.ylabel('Counts')
-    plt.title(f'Distribution of cluster sizes for Model {model}')
+    plt.title(f'Distribution of cluster sizes for Model {model} - fixed')
     plt.xticks((np.arange(0, max(cs_list_step_1)+1, 5)).tolist())  # Ensure each cluster size is a tick on the x-axis
     plt.grid(True, alpha=0.5)
 
-    plt.savefig(save_plots_to + f"cs_hist_plot_model_{model}_SS_run_1.png", dpi='figure')
+    plt.savefig(save_plots_to + f"cs_hist_plot_model_{model}_SS_run_1_5.png", dpi='figure') # change this to _5 for last calc.
     plt.show()
 
 """
@@ -142,7 +142,7 @@ def subplots_hist_model_4(cs_list_step_1, counts_list, cs_list_step_1_control, c
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
-
+"""
 ###
 #   First investigation - Models 1, 2 + 3 histrogram plots of steady state cluster sizes
 ###
@@ -172,3 +172,29 @@ for i in range(1, 9):
     model_i_cs_list_step_1_control, model_i_counts_list_control = get_counts_and_sizes(model_i_cs_counter_control, no_of_frames_4_control)
 
     subplots_hist_model_4(model_i_cs_list_step_1, model_i_counts_list, model_i_cs_list_step_1_control, model_i_counts_list_control, i, colors[i-1])
+
+
+
+###
+#   Third investigation - Models 5, 6 + 7 histrogram plots of steady state cluster sizes
+###
+
+trimmed_outfiles_cs_list_567 = [f'trimmed_outfile_cs_{i}_run_1.dat' for i in range(5, 8)]
+print(trimmed_outfiles_cs_list_567)
+
+for i in range(1, 4):
+    model_i_cs_counter, no_of_frames_567 = count_cluster_sizes(trimmed_outfiles_cs_list_567[i-1])
+    model_i_cs_list_step_1, model_i_counts_list = get_counts_and_sizes(model_i_cs_counter, no_of_frames_567)
+    
+    plot_histogram_models_123(model_i_cs_list_step_1, model_i_counts_list, (i+4), colors[i-1])
+"""
+
+
+trimmed_outfiles_cs_list_567_5 = [f'trimmed_outfile_cs_{i}_run_1_5.dat' for i in range(5, 8)]
+
+# parse data
+for i in range(1, 4):
+    model_i_cs_counter, no_of_frames_567_5 = count_cluster_sizes(trimmed_outfiles_cs_list_567_5[i-1])
+    model_i_cs_list_step_1, model_i_counts_list = get_counts_and_sizes(model_i_cs_counter, no_of_frames_567_5)
+    
+    plot_histogram_models_123(model_i_cs_list_step_1, model_i_counts_list, (i+4), colors[i-1])

plots_code/plots_567_outfiles.py
@@ -8,10 +8,10 @@ import numpy as np
 local_or_cplab = input("Local or cplab: ")
 
 if local_or_cplab == "local":
-    path_to_trimmed_outfiles = '/home/elenaespinosa/OneDrive/Uni/Summer_courses/Summer_project/mod_lammps_code/outfiles/'
+    path_to_trimmed_outfiles = '/home/elenaespinosa/OneDrive/Uni/Summer_courses/Summer_project/mod_lammps_code/outfiles/trimmed_outfiles/'
     save_plots_to = '/home/elenaespinosa/OneDrive/Uni/Summer_courses/Summer_project/mod_lammps_code/plots/steady_state/'
 else:
-    path_to_trimmed_outfiles = '/home/s2205640/Documents/summer_project/mod_lammps_code/outfiles/' # removed the path to the TRIMMED OUTFILES!!! ### CHANGE THIS LATER ###
+    path_to_trimmed_outfiles = '/home/s2205640/Documents/summer_project/mod_lammps_code/outfiles/trimmed_outfiles/'
     save_plots_to = '/home/s2205640/Documents/summer_project/mod_lammps_code/plots/steady_state/'
 
 
@@ -19,8 +19,7 @@ def calc_stats(data_frame):
 
     mean = data_frame.mean()
     std = data_frame.std() # sample standard deviation, normalized by 1 / sqrt(N-1)
-    sem = std / np.sqrt(data_frame.count()) # data_frame.count() = N = 201
-    print(data_frame.count())
+    sem = std / np.sqrt(data_frame.count()) # data_frame.count() = N = 1401
 
     return mean, std, sem
 
@@ -29,7 +28,7 @@ def calc_stats(data_frame):
 #   Third investigation - Model 5, 6 + 7 plots
 ###
 
-trimmed_outfiles_list_567 = [f'outfile_{i}_run_1.dat' for i in range(5, 8)] # add "trimmed_" bit at the start when doing it on trimmed outfiles!
+trimmed_outfiles_list_567 = [f'trimmed_outfile_{i}_run_1.dat' for i in range(5, 8)]
 
 # dictionary to store data frames
 data_frames_567_trimmed = {}
@@ -69,39 +68,7 @@ data_frame_name = ['No_of_clusters', 'Mean_size_of_clusters',
 x_pos = np.arange(len(models))
 
 
-
-
-
-def plots(data_frame_name, column_name, column_no):
-    plt.figure(figsize=(16, 10))
-
-    plt.plot(data_frames_567_trimmed[1]['Timesteps'], data_frames_567_trimmed[1][data_frame_name], marker='.', alpha=0.7, label='Model 5')
-    plt.plot(data_frames_567_trimmed[2]['Timesteps'], data_frames_567_trimmed[2][data_frame_name], marker='.', alpha=0.7, label='Model 6')
-    plt.plot(data_frames_567_trimmed[3]['Timesteps'], data_frames_567_trimmed[3][data_frame_name], marker='.', alpha=0.7, label='Model 7')
-
-    plt.xlabel('Timesteps', fontsize ='16')
-    plt.xticks(fontsize='14')
-
-    plt.ylabel(f'{column_name}', fontsize ='16')
-    plt.yticks(fontsize='14')
-
-    plt.title(f'{column_name} vs Timesteps', fontsize ='16')
-    plt.ticklabel_format(style='plain')
-
-    plt.legend(fontsize="14")
-    plt.grid(True)
-
-    plt.savefig(save_plots_to + f"plot_{column_no}_model_5_run_1.png", dpi='figure')
-    #plt.show()
-
-for i, column in enumerate(column_name):
-    plots(data_frame_name[i], column_name[i], (i+1))
-
-
-
-
-
-def plots_models_567(mean_567, std_567, sem_567, column_name, column_no):
+def SS_plots_models_567(mean_567, std_567, sem_567, column_name, column_no):
     bar_labels_mean = [f'{model}: {mean_567[i]:.2f} ± {sem_567[i]:.2f} (1 SEM)' for i, model in enumerate(models)]
     bar_labels_std = [f'{model}: {std_567[i]:.2f} ± ---' for i, model in enumerate(models)]
 
@@ -109,7 +76,7 @@ def plots_models_567(mean_567, std_567, sem_567, column_name, column_no):
 
     fig.supxlabel('Models', fontsize ='16')
     fig.supylabel(column_name, fontsize ='16')
-    fig.suptitle(f'{column_name} for different models - Mean ± 1 SEM (left) and STD (right)', fontsize='16')
+    fig.suptitle(f'{column_name} for different models - Mean ± 1 SEM (left) and STD (right) - fixed', fontsize='16')
 
     left_bar = axs[0].bar(models, mean_567, yerr=sem_567, capsize=2, label=bar_labels_mean, color=colors[:4])
 
@@ -126,17 +93,68 @@ def plots_models_567(mean_567, std_567, sem_567, column_name, column_no):
     axs[1].legend(fontsize=14)
     axs[1].grid(True, alpha=0.5)
 
-    plt.savefig(save_plots_to + f"plot_{column_no}_model_567_SS_run_1.png", dpi='figure')
-    #plt.show()
+    plt.savefig(save_plots_to + f"plot_{column_no}_model_567_SS_run_1_5.png", dpi='figure') # change this to _5 for last calc.
+    plt.show()
 
 
-
-# plots 1 to 8 for models 5, 6 and 7 - Mean ± 1 SEM (left) and STD (right)
+# steady state plots 1 to 8 for models 5, 6 and 7 - Mean ± 1 SEM (left) and STD (right) - proteins being type 4 only
 """
 for i, column in enumerate(column_name):
     mean_567, std_567, sem_567 = get_stats_567(data_frames_567_trimmed, data_frame_name[i])
-    plots_models_567(mean_567, std_567, sem_567, column_name[i], (i+1))
+    SS_plots_models_567(mean_567, std_567, sem_567, column_name[i], (i+1))
 """
+
+
+
+trimmed_outfiles_list_567_5 = [f'trimmed_outfile_{i}_run_1_5.dat' for i in range(5, 8)]
+
+# dictionary to store data frames
+data_frames_567_trimmed_5 = {}
+
+# parse data
+for i in range(1, 4):
+    data_frames_567_trimmed_5[i] = pd.read_csv(path_to_trimmed_outfiles + trimmed_outfiles_list_567_5[i-1], sep=' ', comment='#', header=None)
+    data_frames_567_trimmed_5[i].columns = ['Timesteps', 'No_of_clusters', 'Mean_size_of_clusters', 
+                                        'Size_of_largest_cluster', 'No_of_clusters_of_size_1', 'No_proteins_bound_to_poly', 
+                                        'Fraction_clusters_bound_to_poly', 'No_type_2_poly_bound_to_prot', 'Mean_no_type_2_in_cluster']
+
+# proteins being type 4 + 5
+
+for i, column in enumerate(column_name):
+    mean_567, std_567, sem_567 = get_stats_567(data_frames_567_trimmed_5, data_frame_name[i])
+    SS_plots_models_567(mean_567, std_567, sem_567, column_name[i], (i+1))
+
+
+
+
+
+def full_plots(data_frame_name, column_name, column_no):
+    plt.figure(figsize=(16, 10))
+
+    plt.plot(data_frames_567_trimmed_5[1]['Timesteps'], data_frames_567_trimmed_5[1][data_frame_name], marker='.', alpha=0.7, label='Model 5')
+    plt.plot(data_frames_567_trimmed_5[2]['Timesteps'], data_frames_567_trimmed_5[2][data_frame_name], marker='.', alpha=0.7, label='Model 6')
+    plt.plot(data_frames_567_trimmed_5[3]['Timesteps'], data_frames_567_trimmed_5[3][data_frame_name], marker='.', alpha=0.7, label='Model 7')
+
+    plt.xlabel('Timesteps', fontsize ='16')
+    plt.xticks(fontsize='14')
+
+    plt.ylabel(f'{column_name}', fontsize ='16')
+    plt.yticks(fontsize='14')
+
+    plt.title(f'{column_name} vs Timesteps - fixed', fontsize ='16')
+    plt.ticklabel_format(style='plain')
+
+    plt.legend(fontsize="14")
+    plt.grid(True)
+
+    plt.savefig(save_plots_to + f"plot_{column_no}_model_5_run_1_5.png", dpi='figure')
+    plt.show()
+
+
+for i, column in enumerate(column_name):
+    full_plots(data_frame_name[i], column_name[i], (i+1))
+
+
 
 
 